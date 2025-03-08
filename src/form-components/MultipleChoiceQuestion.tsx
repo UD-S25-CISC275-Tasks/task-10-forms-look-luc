6425cd035b41b9ae041b82e53552e25e6e848659
Luc De Nardi <denardiluc@gmail.com>
2025-03-08 11:12:50 -0500

    finished task 10

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -8,9 +9,33 @@ export function MultipleChoiceQuestion({
     expectedAnswer: string;
 }): React.JSX.Element {
     const [curr, setCurr] = useState<string>(options[0]);
+
+    function updateCurr(event: React.ChangeEvent<HTMLSelectElement>) {
+        setCurr(event.target.value);
+    }
+
+    function checkCurr() {
+        if (curr === expectedAnswer) {
+            return <span>✔️</span>;
+        } else {
+            return <span>❌</span>;
+        }
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userEmotions">
+                <Form.Label>How do you feel?</Form.Label>
+                <Form.Select value={curr} onChange={updateCurr}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{checkCurr()}</div>
         </div>
     );
 }
